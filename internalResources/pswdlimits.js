1b3a5f96852494b1bba04ff9e8dd8745fcfc4495
cedr00m4k108 <cedr00m4k108@icloud.com>
2025-11-22 09:59:56 -0600

    Correction to invalid character range error

--- internalResources/pswdlimits.js
+++ internalResources/pswdlimits.js
@@ -1,9 +1,9 @@
 function certainChars(inputElement) {
-	const chars = /[^0-9A-Za-z+-*/$%# =]/g;
+	const chars = /[^0-9A-Za-z\\\+\-\*\/\$\%\#\ \=]/g;
 	inputElement.value = inputElement.value.replace(chars, '');
 }
 function certainCharsHex(inputElement) {
-	const charshex = /[^0-9A-Fa-f+-*/$%#=]/g;
+	const charshex = /[^0-9A-Fa-f\\\+\-\*\/\$\%\#\=]/g;
 	inputElement.value = inputElement.value.replace(charshex, '');
 }
 function pswdusrGo() {
